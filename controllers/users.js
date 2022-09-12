906a7e6e269cd11640bc3d83b2256819b90a5e34
weston-bailey <wdbailey87@gmail.com>
2022-09-12 13:57:50 -0700

    finish step 2

--- controllers/users.js
+++ controllers/users.js
@@ -23,20 +23,56 @@ router.post('/', async (req, res) => {
     }
 })
 
+//http://127.0.0.1:3000/users/login?message=Incorrect%20username%20or%20password
 // GET /users/login -- show a login form to the user
 router.get('/login', (req, res) => {
-    res.render('users/login.ejs')
+    console.log(req.query)
+    res.render('users/login.ejs', {
+        // if the req.query.message exists, pass it is as the message, otherwise pass in null
+        // ternary operator
+        // condition ? expression if truthy : expression if falsy
+        message: req.query.message ? req.query.message : null
+    })
 })
 
 // POST /users/login -- accept a payload of form data and use it log a user in 
-router.post('/login', (req, res) => {
-    console.log(req.body)
-    res.send('log the user in')
+router.post('/login', async (req, res) => {
+    try {
+        // look up the user in the db using the supplied email
+        const user = await db.user.findOne({ 
+            where: {
+                email: req.body.email
+            } 
+        })
+        const noLoginMessage = 'Incorrect username or password'
+
+        // if the user is not found -- send the user back to the login form
+        if (!user) {
+            console.log('user not found')
+            res.redirect('/users/login?message=' + noLoginMessage)
+        // if the user is found but has given the wrong password -- send them back to the login form
+        } else if (user.password !== req.body.password) {
+            console.log('wrong password')
+            res.redirect('/users/login?message=' + noLoginMessage)
+        // if the user is found and the supplied password matches what is in the database -- log them in
+        } else {
+            console.log('loggin the user in!!!')
+            res.cookie('userId', user.id)
+            res.redirect('/')
+        }
+    } catch(err) {
+        console.log(err)
+        res.send('server error')
+    }
+
 })
 
 // GET /users/logout -- log out a user by clearing the stored cookie
 router.get('/logout', (req, res) => {
-    res.send('log the user out')
+    // clear the cookie
+    res.clearCookie('userId')
+    // redirect to the homepage
+    res.redirect('/')
 })
 
 module.exports = router
